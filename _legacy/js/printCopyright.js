8cfc88cffdefda6b9bed8f0f1448e500441a5583
Ryan Maelhorn <ryanmaelhorn@gmail.com>
2026-08-11 23:06:31 -0500

    Remove _legacy folder
    Superseded by the current site structure (assets/, css/, js/, train/).

--- _legacy/js/printCopyright.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('footer').innerHTML =
-	`<div style="text-align: center;">
-		<span>
-			&copy;${new Date().getFullYear()} Modest Records Nashville
-		</span>
-	</div>`;
-
-	//  · <a href="/contact">contact</a>
